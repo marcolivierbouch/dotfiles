b326d0096bbbb5c85a66a68d6aa4e1ad8a27a19b
Marc-Olivier Bouchard <mo.bouchard1997@gmail.com>
2018-05-26 19:42:11 -0400

    Add files via upload

--- config/fish/lol.fish
+++ config/fish/lol.fish
@@ -119,7 +119,7 @@ function fish_prompt
     end
 
     # finally print the prompt
-    lolfish $USER '@' (hostname -s) ':' $current_dir $git_dir $error $prompt ' '
+    lolfish $USER $current_dir $git_dir $error $prompt ' '
 end
 
 # Right side prompt
